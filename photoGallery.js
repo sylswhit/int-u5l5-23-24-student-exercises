b79f95b7a119a3da133714ab7d0c13f80535f90d
sylswhit <sylastw@att.net>
2026-06-24 13:37:48 -0500

    done

--- photoGallery.js
+++ photoGallery.js
@@ -1,13 +1,17 @@
 // 1. Declare a variable name 'imageContainer' and store the html element with the id 'imageContainer'
 //  - Using querySelector, store the selector #imageContainer
-
+let imageContainer = document.querySelector(#imageContainer);
 
 
 // 2. Add at least four image urls to the imageUrls array
-let imageUrls = [];
+let imageUrls = ["abc.png", "sylas.jpeg", "nathan.png", "xyz.jpeg"];
 
 // 3. Selecting the imageUrls array, create a forEach loop.
-
+imageUrls.forEach(function (urls) {
+    let image = document.createElement("img");
+    image.src = urls;
+    imageContainer.appendChild(image);
+});
 
 // In side the body of the loop:
 //  - Create an image element and store it in a variable named 'image'
